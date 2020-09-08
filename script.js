ce79489d5b0eba85693d554a6ea7c88087eb12aa
Daniel <65277548+Daniel4-Scratch@users.noreply.github.com>
2020-09-08 11:39:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function submit(){
+document.getElementById('if').src=
+"https://snap.berkeley.edu/embed?project="+
+document.getElementById('txt1').value+
+"&user="+
+document.getElementById('txt2').value+
+"&showTitle=true&showAuthor=true&editButton=true&pauseButton=true"
+}
