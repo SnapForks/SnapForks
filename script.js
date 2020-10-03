a81ba058322ea8c882f00155a9740ded30aab01e
Daniel <mineminecraftpe687@gmail.com>
2020-10-03 13:33:27 +1000

    Improve JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Player Script
 function submit(){
 document.getElementById('if').src=
 "https://snap.berkeley.edu/embed?project="+
@@ -5,14 +6,14 @@ document.getElementById('txt1').value+
 "&user="+
 document.getElementById('txt2').value+
 "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
-
+//Embed Script
 var x = document.getElementById('code'); 
 x.value =  "https://snap.berkeley.edu/embed?project="+
 document.getElementById('txt1').value+
 "&user="+
 document.getElementById('txt2').value+
 "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
-
+//Error Script
 }
 function errorFive(){
   alert("Error")
