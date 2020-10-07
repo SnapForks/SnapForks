c4b3ceaf26af3059bdc8a632786a89062f413000
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-07 11:44:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 //Player Script
+const iframe = document.getElementById('iframe');
+const user = document.getElementById('project-author');
+const project = document.getElementById('project-name');
+const projectURLTextarea = document.getElementById('code')
 function submit(){
-document.getElementById('if').src=
-"https://snap.berkeley.edu/embed?project="+
-document.getElementById('txt1').value+
-"&user="+
-document.getElementById('txt2').value+
-"&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
-//Embed Script
-var x = document.getElementById('code'); 
-x.value =  "https://snap.berkeley.edu/embed?project="+
-document.getElementById('txt1').value+
-"&user="+
-document.getElementById('txt2').value+
-"&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
-//Error Script
+  const encodedUsername = encodeURIComponent(user.value);
+  const encodedProjectName = encodeURIComponent(project.value)
+  iframe.src=`https://snap.berkeley.edu/embed?project=${encodedProjectName}&user=${encodedUsername}&showTitle=true&showAuthor=true&editButton=true&pauseButton=true`; 
+  projectURLTextarea.value =  `https://snap.berkeley.edu/embed?project=${encodedProjectName}user=${encodedUsername}&showTitle=true&showAuthor=true&editButton=true&pauseButton=true`;
 }
 function errorFive(){
   alert("Error")
