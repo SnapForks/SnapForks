0a5ccde4eda1a2ff1deb68baaebc6a2ecb7330d9
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-08 11:42:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 function submit(){
 document.getElementById('if').src=
 "https://snap.berkeley.edu/embed?project="+
-document.getElementById('txt1').value+
+encodeURIComponent(document.getElementById('txt1').value)+
 "&user="+
-document.getElementById('txt2').value+
+encodeURIComponent(document.getElementById('txt2').value)+
 "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
 //Embed Script
 var x = document.getElementById('code'); 
 x.value =  "https://snap.berkeley.edu/embed?project="+
-document.getElementById('txt1').value+
+encodeURIComponent(document.getElementById('txt1').value)+
 "&user="+
-document.getElementById('txt2').value+
+encodeURIComponent(document.getElementById('txt2').value)+
 "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
 //Error Script
 }
