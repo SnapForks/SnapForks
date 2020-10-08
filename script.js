b8d7d97fcbf8db488eb0dd29db423bb79ef65624
R4356th <61620631+R4356th@users.noreply.github.com>
2020-10-08 14:30:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-
-var project = document.getElementById('txt1').value;
-var projectnametocheckfor = project.str.replace(/#/g, "%23");
-
 //Player Script
 
 function submit(){
 document.getElementById('if').src=
 "https://snap.berkeley.edu/embed?project="+
-projectnametocheckfor +
+encodeURIComponent(document.getElementById('txt1').value) +
 "&user="+
 document.getElementById('txt2').value+
 "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
 //Embed Script
 var x = document.getElementById('code'); 
 x.value =  "https://snap.berkeley.edu/embed?project="+
-document.getElementById('txt1').value+
+encodeURIComponent(document.getElementById('txt1').value) +
 "&user="+
 document.getElementById('txt2').value+
 "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true";
+  
 //Error Script
 }
 function errorFive(){
