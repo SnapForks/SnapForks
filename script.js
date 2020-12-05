2b97c2cbfc9d16e06f7f52ebae74bc030dcd5fe2
programmeruser2 <69394951+programmeruser2@users.noreply.github.com>
2020-12-04 21:15:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 ;(function () {
 let username, project, url;
+const errElem = document.getElementById('err-msg'); 
 document.getElementById('submitBtn').addEventListener('click', async function () {
   project = encodeURIComponent(document.getElementById('txt1').value);
   username = encodeURIComponent(document.getElementById('txt2').value);
@@ -16,9 +17,11 @@ document.getElementById('submitBtn').addEventListener('click', async function ()
     alert('Error ' + req.status + ' while fetching project');
 });
 function error(msg) {
-  
+  errElem.style.display = 'inline-block';
+  errElem.innerText = msg;
 }
-function loadProject() {  
+function loadProject() {
+  errElem.style.display = 'none';
   url=`https://snap.berkeley.edu/embed?project=${project}&user=${username}&showTitle=true&showAuthor=true&editButton=true&pauseButton=true`;
   document.getElementById('if').src = url;
   document.getElementById('code').value = url;
